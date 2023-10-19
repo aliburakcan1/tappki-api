9b87c02a9cae7f726910d25f7723c61ad02a3501
unknown <aliburakcan1@gmail.com>
2023-10-19 17:24:04 +0300

    import meilisearch

--- video_retriever.py
+++ video_retriever.py
@@ -1,10 +1,10 @@
 from config import MEILISEARCH_MASTER_KEY
 import os
-#import meilisearch
+import meilisearch
 import subprocess
 import time
 from pymongo import MongoClient
-from config import ATLAS_USERNAME, ATLAS_PASSWORD, ATLAS_DATABASE
+from config import ATLAS_USERNAME, ATLAS_PASSWORD, ATLAS_DATABASE, MEILISEARCH_MASTER_KEY, MEILISEARCH_HOSTNAME
 import random
 from loguru import logger
 
@@ -14,25 +14,32 @@ class VideoRetriever:
     def __init__(self, mongo_db, mongo_db_collection) -> None:
         #subprocess.Popen(["./meilisearch", f"--master-key={MEILISEARCH_MASTER_KEY}"])
         #time.sleep(1)
-        #self.msearch_client = meilisearch.Client('http://localhost:7700', MEILISEARCH_MASTER_KEY)
+        self.msearch_client = meilisearch.Client(MEILISEARCH_HOSTNAME, MEILISEARCH_MASTER_KEY)
         #print(self.msearch_client.version())
         self.mongo_uri = f"mongodb+srv://{ATLAS_USERNAME}:{ATLAS_PASSWORD}@{ATLAS_DATABASE}.mongodb.net/?retryWrites=true&w=majority"
         self.mongo_client = MongoClient(self.mongo_uri)
         self.mongo_db = self.mongo_client[mongo_db]
         self.mongo_db_collection = self.mongo_db[mongo_db_collection]
-        #self.mongo_db_query = {"title": {"$ne": ""}}
-        #self.mongo_db_sort = [("_id", -1)]
-        #self.mongo_db_project = {"_id": 0}
-        #self.mongo_db_documents = self.mongo_db_collection.find(self.mongo_db_query, sort=self.mongo_db_sort, projection=self.mongo_db_project)
-        #self.msearch_documents = list(self.mongo_db_documents)
-        #self.index.add_documents(documents=documents)
-        #self.msearch_client.delete_index("videos")
-        #self.msearch_index = self.msearch_client.index('videos')
-        #self.msearch_index.add_documents(self.msearch_documents)
+        self.mongo_db_query = {"title": {"$ne": ""}}
+        self.mongo_db_sort = [("_id", -1)]
+        self.mongo_db_project = {"_id": 0}
+        self.mongo_db_documents = self.mongo_db_collection.find(self.mongo_db_query, sort=self.mongo_db_sort, projection=self.mongo_db_project)
+        self.msearch_documents = list(self.mongo_db_documents)
+        #self.index.add_documents(documents=self.msearch_documents)
+        self.msearch_client.delete_index("annotation_index")
+        self.msearch_index = self.msearch_client.index('annotation_index')
+        self.msearch_index.add_documents(self.msearch_documents)
         #print(self.msearch_client.get_task(0))
     
     @logger.catch
     def search(self, query, X_Session_Id):
+        logger.info(f"Session: {X_Session_Id} | query is being searched: {query}")
+        documents = self.msearch_index.search(query, {"limit": 100})
+        #logger.info(f"documents are retrieved: {documents}")
+        return documents['hits']
+    
+    @logger.catch
+    def search_mongodb(self, query, X_Session_Id):
         logger.info(f"Session: {X_Session_Id} | query is being searched: {query}")
         documents = self.mongo_db_collection.aggregate([
                 {
